b5ea96d4c303e8f080ef1be4a4d98b2a80c4e2e2
HeberBarra <heberferreirabarra@gmail.com>
2024-02-15 15:40:26 -0300

    fixed merge error

--- NeoVim/.config/nvim/lua/custom/plugins.lua
+++ NeoVim/.config/nvim/lua/custom/plugins.lua
@@ -2,8 +2,7 @@ local plugins = {
     {
         "nvim-treesitter/nvim-treesitter",
         opts = {
-        ensure_installed = { "vim", "lua", "html", "css", "javascript", "typescript", "tsx", "json", "yaml", "csv", "python", "dockerfile", "c", "c_sharp", "cpp", "gitignore", "java", "bash", "sql"
-            },
+        ensure_installed = { "vim", "lua", "html", "css", "javascript", "typescript", "tsx", "json", "yaml", "csv", "python", "dockerfile", "c", "c_sharp", "cpp", "gitignore", "java", "bash", "sql"},
         },
     },
     {
@@ -16,11 +15,7 @@ local plugins = {
     {
         "williamboman/mason.nvim",
         opts = {
-<<<<<<< HEAD
             ensure_installed = { "bash-language-server", "clangd", "csharp-language-server","css-lsp", "cssmodules-language-server","html-lsp", "jdlts", "lua-language-server", "prettier", "pyright", "sqlls","stylua" },
-=======
-            ensure_installed = { "bash-language-server", "clangd", "csharp-language-server","css-lsp", "cssmodules-language-server","html-lsp", "jdtls", "lua-language-server", "prettier", "pyright","stylua" },
->>>>>>> 665a96fbf600f9642ac209c7998e186f8f38f101
         },
     }
 }
