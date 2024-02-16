2075385a49322a596e3952bb77e06d651e67848f
HeberBarra <heberferreirabarra@gmail.com>
2024-02-16 10:18:58 -0300

    autocmd for specific configurations per filetype

--- NeoVim/.config/nvim/lua/custom/init.lua
+++ NeoVim/.config/nvim/lua/custom/init.lua
@@ -1,3 +1,16 @@
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
+
+local patterns = {"html", "json", "lua", "xml"}
+
+for _, pattern in ipairs(patterns) do
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = pattern,
+    callback = function ()
+      vim.opt.shiftwidth = 2
+      vim.opt.tabstop = 2
+      vim.opt.softtabstop = 2
+    end
+  })
+end
