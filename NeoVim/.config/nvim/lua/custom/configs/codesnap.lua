9b4f96cdc2edfb026c406ea0f55fe0be502d476e
Heber F Barra <heberferreirabarra@gmail.com>
2024-04-05 10:21:27 -0300

    added check to config file

--- NeoVim/.config/nvim/lua/custom/configs/codesnap.lua
+++ NeoVim/.config/nvim/lua/custom/configs/codesnap.lua
@@ -1,4 +1,10 @@
-require("codesnap").setup({
+local present, codesnap = pcall(require, "codesnap")
+
+if not present then
+  return
+end
+
+codesnap.setup({
   bg_theme = "grape",
   has_breadcrumbs = true,
   code_font_family = "JetBrainsMono Nerd Font Mono"
