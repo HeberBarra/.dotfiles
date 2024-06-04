2fb9e4832f40e97f082bef62f734b7cae1d0b5e6
Heber F Barra <heberferreirabarra@gmail.com>
2024-06-04 11:23:14 -0300

    updated trouble.nvim config

--- config/nvim/lua/configs/trouble.lua
+++ config/nvim/lua/configs/trouble.lua
@@ -4,37 +4,4 @@ if not present then
   return
 end
 
-trouble.setup({
-  position = "bottom",
-  height = 15,
-  width = 30,
-  icons = true,
-  mode = "workspace_diagnostics",
-  severity = nil,
-  fold_open = "🗁",
-  fold_closed = "🖿",
-  group = true,
-  padding = true,
-  cycle_results = true,
-  action_keys = {
-    close = "q",
-    cancel = "<esc>",
-    refresh = "<c-r>",
-    open_split = { "<c-x>" },
-    open_vsplit = { "<c-v>" },
-    toggle_mode = "m",
-    toggle_preview = "p",
-    switch_severity = "s",
-    previous = "k",
-    next = "j"
-  },
-  multiline = true,
-  ident_lines = true,
-  signs = {
-    error = "",
-    warning = "",
-    hint = "",
-    information = "",
-    other = "",
-  }
-})
+trouble.setup({})
--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -140,13 +140,43 @@ local plugins = {
     dependencies = { 'nvim-telescope/telescope.nvim' },
     lazy = true,
   },
-  {
-    "folke/trouble.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function ()
-      require("configs.trouble")
-    end
-  },
+	{
+		"folke/trouble.nvim",
+		opts = {}, -- for default options, refer to the configuration section for custom setup.
+		cmd = "Trouble",
+		keys = {
+			{
+				"<leader>xx",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "Diagnostics (Trouble)",
+			},
+			{
+				"<leader>xX",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				desc = "Buffer Diagnostics (Trouble)",
+			},
+			{
+				"<leader>cs",
+				"<cmd>Trouble symbols toggle focus=false<cr>",
+				desc = "Symbols (Trouble)",
+			},
+			{
+				"<leader>cl",
+				"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+				desc = "LSP Definitions / references / ... (Trouble)",
+			},
+			{
+				"<leader>xL",
+				"<cmd>Trouble loclist toggle<cr>",
+				desc = "Location List (Trouble)",
+			},
+			{
+				"<leader>xQ",
+				"<cmd>Trouble qflist toggle<cr>",
+				desc = "Quickfix List (Trouble)",
+			},
+		},
+	},
   {
     "folke/which-key.nvim",
     event = "VeryLazy",
