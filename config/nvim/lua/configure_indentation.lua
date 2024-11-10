d04a57cbe93bd51eb0e63e41eb7ab0456899b143
Heber F Barra <heberferreirabarra@gmail.com>
2024-11-10 12:26:17 -0300

    Configure indentation for make file

--- config/nvim/lua/configure_indentation.lua
+++ config/nvim/lua/configure_indentation.lua
@@ -1,5 +1,6 @@
 local two_spaces_indentation = { "bib", "html", "json", "jsonc", "tex", "lua", "toml", "sql", "xml" }
 local four_spaces_indentation = { "asm", "c", "cpp", "cs", "java", "python", "javascript", "typescript" }
+local tab_indentation = { "make" }
 
 local set_indentation_settings = function(filetypes, space_quantity)
   for _, filetype in ipairs(filetypes) do
@@ -16,3 +17,15 @@ end
 
 set_indentation_settings(two_spaces_indentation, 2)
 set_indentation_settings(four_spaces_indentation, 4)
+
+for _, filetype in ipairs(tab_indentation) do
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = filetype,
+    callback = function()
+      vim.opt.shiftwidth = 4
+      vim.opt.tabstop = 4
+      vim.opt.softtabstop = 4
+      vim.opt.expandtab = false
+    end,
+  })
+end
