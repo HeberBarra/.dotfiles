26563a0967de4e4c876332b40ee38da915846ce7
Heber F Barra <heberferreirabarra@gmail.com>
2025-02-15 19:54:29 -0300

    [ NEOVIM ] Removed unnecessary formatters

--- config/nvim/lua/plugins/conform.lua
+++ config/nvim/lua/plugins/conform.lua
@@ -4,12 +4,9 @@ return {
   config = function()
     require("conform").setup({
       formatters_by_ft = {
-        c = { "clang-format" },
-        cpp = { "clang-format" },
         cs = { "csharpier" },
         javascrpt = { "biome" },
         lua = { "stylua" },
-        sql = { "sql-formatter" },
         mysql = { "sql-formatter" },
         python = { "autopep8" },
         typescript = { "biome" },
